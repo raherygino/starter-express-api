e0046f6b98142c35c26191e94fab39491f89d0fe
raherygino <georgino197@gmail.com>
2023-06-27 05:48:46 +0300

    Connect to database

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
+require('dotenv').config();
 const express = require('express');
-const dotenv = require('dotenv').config();/*
-const { errorHandler } = require('./invoice-app-api/middleware/errorMiddleware');
-const connectDB = require('./invoice-app-api/config/db');
-const UploadFile = require('./invoice-app-api/services/uploadFile');
-const multer = require('multer');*/
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
-
-//connectDB();
 const app = express();
-const router = express.Router()
-/*
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-});
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/api/users', require('./invoice-app-api/routes/userRoutes'));
-app.use('/api/organizations', require('./invoice-app-api/routes/organizationRoutes'));
-app.use('/api/categories', require('./invoice-app-api/routes/categoryRoutes'))
-app.use('/api/subcategories', require('./invoice-app-api/routes/subCategoryRoutes'))
-app.use('/api/products', require('./invoice-app-api/routes/productRoutes'))
-app.use('/api/files', require('./invoice-app-api/routes/file.routes'))
-app.use('/api/images', express.static(path.join(__dirname, 'uploads')))
-*/
-router.get('/api', async (req, res) => {
-    res.status(200).json({message: 'Georginot'})
-})
-/*
-router.post('/fileupload', async (req, res) => {
-  try {
-    await UploadFile(req, res);
-    res.status(200).send({"msf": "Dd"})
-  } catch (error) {
-    console.log(error)
-  }
-})
-*/
-app.use(router)
-/*
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
+/* Connect to Database */
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 }
 
-app.use(errorHandler);
-*/
-app.listen(port, () => console.log(`Server started on port ${port}`));
+connectDB();
---
+++ invoice-app-api/index.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const fs = require("fs");
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./invoice-app-api/middleware/errorMiddleware');
+const connectDB = require('./invoice-app-api/config/db');
+const UploadFile = require('./invoice-app-api/services/uploadFile');
+const multer = require('multer');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+const router = express.Router()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/users', require('./invoice-app-api/routes/userRoutes'));
+app.use('/api/organizations', require('./invoice-app-api/routes/organizationRoutes'));
+app.use('/api/categories', require('./invoice-app-api/routes/categoryRoutes'))
+app.use('/api/subcategories', require('./invoice-app-api/routes/subCategoryRoutes'))
+app.use('/api/products', require('./invoice-app-api/routes/productRoutes'))
+app.use('/api/files', require('./invoice-app-api/routes/file.routes'))
+app.use('/api/images', express.static(path.join(__dirname, 'uploads')))
+
+router.get('/api', async (req, res) => {
+    res.status(200).json({message: 'Georginot'})
+})
+
+router.post('/fileupload', async (req, res) => {
+  try {
+    await UploadFile(req, res);
+    res.status(200).send({"msf": "Dd"})
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.use(router)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
