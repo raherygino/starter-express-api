54009efd4a8d9796a4d0804e278c5ae2c916bd01
Gino <111128142+raherygino@users.noreply.github.com>
2023-11-17 02:15:41 +0300

    Update index.js to init commit

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-/* Connect to Database */
-mongoose.set('strictQuery', false);
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect('mongodb+srv://georgino197:aKRON7Sc3knYCfVx@gino.3i5ceui.mongodb.net/');
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
-
-connectDB();
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
